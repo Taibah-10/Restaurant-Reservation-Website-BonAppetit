fbab8be62251505eb48c3344778e140b7f072e0d
Taibah-10 <taibahbagwan10@gmail.com>
2023-02-23 12:58:58 +0530

    updated

--- BonAppetit/urls.py
+++ BonAppetit/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.static import serve
+from django.conf.urls import url
 
 admin.site.site_header = "Bon Appetit Admin"
 admin.site.site_title = "Bon Appetit Admin Portal"
@@ -23,4 +25,7 @@ admin.site.index_title = "Welcome to Bon Appetit"
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls'))
+
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
